2095911ba129b36fcfa2325a114a90f79f60e2fe
Usuario <Usuario@DESKTOP-SJR6E1A>
2020-09-29 12:56:08 -0300

    Botones

--- TaTeToro/src/interfaz/Interfaz.java
+++ TaTeToro/src/interfaz/Interfaz.java
@@ -3,6 +3,7 @@ package interfaz;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
 
 public class Interfaz {
 
@@ -36,8 +37,44 @@ public class Interfaz {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 340);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JButton btn_1 = new JButton("1");
+		btn_1.setBounds(115, 47, 60, 60);
+		frame.getContentPane().add(btn_1);
+		
+		JButton btn_2 = new JButton("2");
+		btn_2.setBounds(185, 47, 60, 60);
+		frame.getContentPane().add(btn_2);
+		
+		JButton btn_3 = new JButton("3");
+		btn_3.setBounds(255, 47, 60, 60);
+		frame.getContentPane().add(btn_3);
+		
+		JButton btn_4 = new JButton("4");
+		btn_4.setBounds(115, 118, 60, 60);
+		frame.getContentPane().add(btn_4);
+		
+		JButton btn_5 = new JButton("5");
+		btn_5.setBounds(185, 118, 60, 60);
+		frame.getContentPane().add(btn_5);
+		
+		JButton btn_6 = new JButton("6");
+		btn_6.setBounds(255, 118, 60, 60);
+		frame.getContentPane().add(btn_6);
+		
+		JButton btn_7 = new JButton("7");
+		btn_7.setBounds(115, 189, 60, 60);
+		frame.getContentPane().add(btn_7);
+		
+		JButton btn_8 = new JButton("8");
+		btn_8.setBounds(185, 189, 60, 60);
+		frame.getContentPane().add(btn_8);
+		
+		JButton btn_9 = new JButton("9");
+		btn_9.setBounds(255, 189, 60, 60);
+		frame.getContentPane().add(btn_9);
 	}
-
 }
