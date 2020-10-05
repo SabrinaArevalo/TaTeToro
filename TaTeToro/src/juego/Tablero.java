bbfab78742c2a5995566289f9afdb3175318f89c
sabri <sabri@DESKTOP-GRUEQNT>
2020-10-05 16:53:00 -0300

    es prueba

--- TaTeToro/src/juego/Tablero.java
+++ TaTeToro/src/juego/Tablero.java
@@ -3,8 +3,8 @@ package juego;
 public class Tablero {
 	private int[][] tablero;
 	
-	//La idea es tener una matriz de 3x3, y en cada turno poner 1 o 2 según dónde juegue el jugador
-	//Y verificar si ganó a partir de este tablero
+	//La idea es tener una matriz de 3x3, y en cada turno poner 1 o 2 segun donde juegue el jugador
+	//Y verificar si gano a partir de este tablero
 	public Tablero() {
 		tablero=new int[3][3];
 		
